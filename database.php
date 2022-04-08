2116db4cfbed35aeee3ddc7d25dce37f6994d469
Luke Smith <98189885+CSLuke1718@users.noreply.github.com>
2022-04-08 16:19:38 -0700

    correcting sql table

--- database.php
+++ database.php
@@ -4,11 +4,12 @@
 <head>
 	<meta charset = "utf-8">
 	<title>Database form</title>
-	<style type = "text/css">
+	<link rel="stylesheet" href="CSS style.css">
+	<!-- <style type = "text/css">
         table { background-color: lightblue; border-collapse: collapse; border: 1px solid gray; }
         td    { padding: 5px; }
-        tr:nth-child(odd) {background-color: white; } <!-- alternate colors for different rows. -->
-	</style>
+        tr:nth-child(odd) {background-color: white; } <!-- alternate colors for different rows.
+	</style> -->
 </head>
 <body>
 	<?php
@@ -35,21 +36,48 @@
 		
 		mysqli_close( $database );
 	?>
-	<table>
-		<thead>Results of SELECT <?php print("$select") ?> from the Restaurant Database</thead>
+	<table width="100%" border="1">
+  <tbody>
+    <tr>
+      <td colspan="2" style="text-align: center; color: #0E279C;"><h1>The Onkyo Diner</h1></td>
+    </tr>
+	<tr>
+      <td colspan="2" style="text-align: center;">
+      <div class ="topnav" >
+      <a href="ProjHome.html">Home</a>
+      <a href="projmenu.html">Menu</a>
+      <a href="projlocation.html">Location</a>
+	  <a href="projvideo.html">Video</a>
+		<a class="active" href="databaseQuery.html">Database</a>
+		<a href="MailingListForm.php">Subscribe</a>
+		<div class="dropdown" style="">
+			<button class="dropbtn"><a href="#">Assignments </a><i class="fa fa-caret-down"></i></button>
+			<div class="dropdown-content">
+				<a href="#">Assignment 1</a>
+				<a href="#">Assignment 2</a>
+			</div>
+		</div>
+      </div>
+      </td>
+    </tr>
+   </table>
+  </tbody>
+	<table style="margin: auto; text-align: left;">
+		<thead>Results of SELECT <?php print("$select") ?> from the <?php print("$select") ?> table of the Restaurant Database</thead>
+		<tbody>
 		<?php
             //while rows from the query, print them out
 			while ( $row = mysqli_fetch_row( $result ) )
             {
 				print( "<tr>" );
 				foreach ( $row as $value )
-					print( "<td>$value</td>" );
+					print( "<td style=\"fit-content; block-size: fit-content;\">$value</td>" );
 				print( "</tr>" );
             }
 		?>
+		</tbody>
 	</table>
 	<p>Your Search yielded <?php print( mysqli_num_rows( $result )) ?> results.</p>
-	<p>Return to the <a href="Projhome.html">home page</a></p>
 	
 </body>
 </html>
